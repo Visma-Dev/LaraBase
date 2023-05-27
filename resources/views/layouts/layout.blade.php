24f4b7f78efa1377208353ed7dc3ed262378f3fd
VISMA-DEV <visma-dev@mail.ru>
2023-05-27 18:22:10 +0300

    Bootstrap integration

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,19 +5,23 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--  Подключаем Bootstrap-->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"> <!-- указываем путь с помощью хелпера asset -->
+
     <title>Document</title>
 </head>
 <body>
 
-<div>
-    <div>
-        <nav>
-            <ul>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-light bg-light">
+            <ul class="navbar-nav">
                 <!-- Ссылаемся на необходимые нам страницы, благодаря именованным роутам -->
-                <li><a href="{{ route('main.index') }}">Main</a></li>
-                <li><a href="{{ route('post.index') }}">Posts</a></li>
-                <li><a href="{{ route('about.index') }}">About</a></li>
-                <li><a href="{{ route('contact.index') }}">Contacts</a></li>
+                <li class="nav-item"><a href="{{ route('main.index') }}">Main</a></li>
+                <li class="nav-item"><a href="{{ route('post.index') }}">Posts</a></li>
+                <li class="nav-item"><a href="{{ route('about.index') }}">About</a></li>
+                <li class="nav-item"><a href="{{ route('contact.index') }}">Contacts</a></li>
             </ul>
         </nav>
     </div>
